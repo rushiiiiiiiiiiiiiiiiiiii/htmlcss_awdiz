f45215d51eff198d3ad848477f6d9159ef6217bc
Rushikesh Arote <rushikesharote14@gmail.com>
2025-01-18 19:26:11 +0530

    Day11

---
+++ Day11/index11.js
@@ -0,0 +1,64 @@
+//var 
+var name = "Rushi"
+name = "AMAN"  //re-assign
+var name = "Kartik" //re-declare
+console.log(name)
+
+//let
+let age = 12
+age=24 //re-assign
+//let can not be re-declare with same variable name
+console.log(age)
+
+//const
+const isOpen = true
+//let can not be re-assign with same variable name
+//let can not be re-declare with same variable name
+console.log(isOpen)
+
+//Hoisting
+myAge = 20;
+console.log(myAge);
+var myAge;
+//let adn const does not support Hoisting
+
+//typeoff
+var uname = "Rushi"
+console.log(typeof uname, uname)
+
+let roll = 12
+console.log(typeof roll, roll)
+
+const isClose = true
+console.log(typeof isClose, isClose)
+
+//calculations
+var num1= 100
+var num2 = 200
+console.log(num1 + num2)
+
+var num1= 100
+var num2 = 200
+console.log(num1 * num2)
+
+var num1= 100
+var num2 = 200
+console.log(num1 - num2)
+
+var num1= 100
+var num2 = 200
+console.log(num1 / num2)
+
+//operators
+console.log(2==2)
+console.log(2==5)
+
+console.log(2==='2')
+console.log(5===5)
+
+console.log(1!=2)
+console.log(4!=4)
+
+console.log(4!==4)
+console.log('4'!==4)
+
